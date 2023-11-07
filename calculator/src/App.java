b9bbe8a5824766d5279afe0c3b37f7acbaf252c1
Charleskahuho <ckcharles004@gmail.com>
2023-11-07 18:27:47 +0300

    add changed

--- calculator/src/App.java
+++ calculator/src/App.java
@@ -1,10 +1,7 @@
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args){
         System.out.println("Hello, World!");
-        int num1 = 4;
-        int num2 = 6;
-        int sum =add(num1, num2);
-        System.out.println(sum);
+
 
     }
 }
