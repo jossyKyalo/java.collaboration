2c097d62fee97617fea29824ce331e84365cc0df
Charleskahuho <ckcharles004@gmail.com>
2023-11-07 18:05:27 +0300

    repo changed

---
+++ calculator/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        int num1 = 4;
+        int num2 = 6;
+        int sum =add(num1, num2);
+        System.out.println(sum);
+
+    }
+}
