d7073a143f827532bd1352280884d791e836be8b
Charleskahuho <125451515+charleskahuho@users.noreply.github.com>
2023-11-08 11:37:06 +0300

    Subtraction class created

---
+++ src/subtraction.java
@@ -0,0 +1,9 @@
+public class SubtractionCalculator {
+    public int subtract(int num1, int num2) {
+        return num1 - num2;
+    }
+
+    public double subtract(double num1, double num2) {
+        return num1 - num2;
+    }
+}
